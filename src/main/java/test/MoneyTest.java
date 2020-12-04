dc9c4c9fa468d1dfcf02394f419c19142ef2a748
MelvynMercier <43468212+MelvynMercier@users.noreply.github.com>
2020-12-04 15:00:10 +0100

    Commit

---
+++ src/main/java/test/MoneyTest.java
@@ -0,0 +1,33 @@
+package test;
+import static org.junit.Assert.*;
+
+import org.junit.jupiter.api.Test;
+
+import src.Money;
+
+
+public class MoneyTest {
+	
+	private Money money = new Money(10, "EUR");
+
+	@Test
+	public void CheckPositiveAmountTest() {
+		assertTrue(money.Amount() >= 0);
+	}
+	
+	@Test
+	public void ISORuleForCurrencyTest() {
+		assertTrue(money.Currency().length() == 3);
+		assertTrue(money.Currency() == "EUR" 
+				|| money.Currency() == "USD" 
+				|| money.Currency() == "CHF" 
+				|| money.Currency() == "GBP");
+	}
+	
+	@Test
+	public void AdditionTest() {
+		Money moneyToAddEur = new Money(20, "EUR");
+		
+		assertEquals(money.add(moneyToAddEur).Amount(), 30);
+	}
+}
