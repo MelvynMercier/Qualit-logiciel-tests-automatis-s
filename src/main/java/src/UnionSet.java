632e590f7012c97c85c25e0d40fa4d7b6eb2c0d2
MelvynMercier <43468212+MelvynMercier@users.noreply.github.com>
2020-12-09 10:31:29 +0100

    Update UnionSet.java

--- src/main/java/src/UnionSet.java
+++ src/main/java/src/UnionSet.java
@@ -7,10 +7,20 @@ public class UnionSet {
     private Vector vector1;
     private Vector vector2;
 
-    public static Vector<Object> unionSet(Vector<Object> vector1, Vector<Object> vector2) {
-        Vector<Object> result = new Vector<Object>();
+    public static Vector<Object> unionSet(Vector<Object> vector1, Vector<Object> vector2) throws Exception {
+        CheckVector(vector1);
+        CheckVector(vector2);
+    	
+    	Vector<Object> result = new Vector<Object>();
         result.addAll(vector1);
         result.addAll(vector2);
         return result;
+    }
+    
+    //Vérification si un vecteur != null
+    public static void CheckVector(Vector<Object> vector) throws Exception {
+    	if(vector == null) {
+    		throw new Exception("Un des vecteur est null");
+    	}
     }
 }
